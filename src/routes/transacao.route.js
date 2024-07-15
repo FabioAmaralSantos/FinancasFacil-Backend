17e06f44bfee4206c66abd1867783e0d4222c3db
Alexandre <alexandrecferreira11@gmail.com>
2024-07-15 20:01:46 -0300

    Criação da Função de Ler os dados do Banco de Dados (1. Retorna todas as transações|2. Retorna uma transaçao específica pelo ID)

--- src/routes/transacao.route.js
+++ src/routes/transacao.route.js
@@ -2,5 +2,7 @@ const route = require("express").Router();
 const transacaoController = require("../controllers/transacao.controller");
 
 route.post("/", transacaoController.create);
+route.get("/", transacaoController.findAll);
+route.get("/:id", transacaoController.findById);
 
 module.exports = route;
--- src/services/transacao.service.js
+++ src/services/transacao.service.js
@@ -1,7 +1,11 @@
 const transacao = require("../models/transacao-model");
 
 const create = (body) => transacao.create(body);
+const findAll = () => transacao.find(); // Função do mongoose para buscar todos os dados.
+const findById = (id) => transacao.findById(id); // Função do mongoose para buscar um dado especifico pelo ID.
 
 module.exports = {
   create,
+  findAll,
+  findById,
 };
