7e46188fb0d8bee81e69f09d2a09dd5a9bcbf4e7
Alexandre <alexandrecferreira11@gmail.com>
2024-07-16 21:30:15 -0300

    Criando o global_middleware

---
+++ src/middlewares/global_middleware.js
@@ -0,0 +1,30 @@
+const transacaoService = require("../services/transacao.service");
+const mongoose = require("mongoose");
+
+const validId = (req, res, next) => {
+  const id = req.params.id;
+
+  // Método do Mongoose para verificar se um ID é válido
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "ID Inválido." });
+  }
+
+  next();
+};
+
+const validTransacao = async (req, res, next) => {
+  const id = req.params.id;
+
+  const transacao = await transacaoService.findById(id);
+
+  if (!transacao) {
+    return res.status(400).send({ message: "Transação não encontrada." });
+  }
+
+  req.id = id;
+  req.transacao = transacao;
+
+  next();
+};
+
+module.exports = { validId, validTransacao };
--- src/routes/transacao.route.js
+++ src/routes/transacao.route.js
@@ -1,9 +1,10 @@
 const route = require("express").Router();
 const transacaoController = require("../controllers/transacao.controller");
+const { validId, validTransacao } = require("../middlewares/global_middleware");
 
 route.post("/", transacaoController.create);
 route.get("/", transacaoController.findAll);
-route.get("/:id", transacaoController.findById);
-route.patch("/:id", transacaoController.update);
+route.get("/:id", validId, validTransacao, transacaoController.findById);
+route.patch("/:id", validId, validTransacao, transacaoController.update);
 
 module.exports = route;
